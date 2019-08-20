d14cfb50f62215b2dfc9f85289038db7d2bc9401
AngelRSanchez <angelraulsanchez3@gmail.com>
2019-08-20 17:20:58 -0400

    Programa finalizado, en el siguiente commit añadire funciones es6

--- js/game.js
+++ js/game.js
@@ -1,5 +1,3 @@
-
-
 const WIDTH = 400;
 const HEIGHT = 400;
 
@@ -9,3 +7,17 @@ let target = {
 };
 
 let $map = document.querySelector('#map');
+let $distance = document.getElementById('distance');
+let clicks = 0;
+
+$map.addEventListener('click', function(e){
+    clicks++;
+    let distance = getDistance(e, target);
+    let distanceHint = getDistanceHint(distance);
+
+    $distance.innerHTML = `<h1>${distanceHint}</h1>`;
+    
+    if(distance < 20){
+        $distance.innerHTML = `<h1>You has found the treasure in ${clicks} clicks!</h1>`;
+    }
+}); 
