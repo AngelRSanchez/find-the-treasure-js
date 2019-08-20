00549a62c20546fe28754185d67946804b1e04a8
AngelRSanchez <angelraulsanchez3@gmail.com>
2019-08-20 17:39:10 -0400

    Programa finalizado

--- js/game.js
+++ js/game.js
@@ -1,23 +1,27 @@
+// constantes del ancho y alto de la imagen para ubicar las coordenadas
+// Añadimos las variables necesarias
 const WIDTH = 400;
 const HEIGHT = 400;
-
+let $map = document.querySelector('#map');
+let $distance = document.querySelector('#distance');
+let clicks = 0;
 let target = {
     x: getRandomNumber(WIDTH),
     y: getRandomNumber(HEIGHT)
 };
 
-let $map = document.querySelector('#map');
-let $distance = document.getElementById('distance');
-let clicks = 0;
-
-$map.addEventListener('click', function(e){
+// Mediante este listener se captura el evento y se ejecuta el juego
+$map.addEventListener('click', (e) => {
     clicks++;
     let distance = getDistance(e, target);
     let distanceHint = getDistanceHint(distance);
 
     $distance.innerHTML = `<h1>${distanceHint}</h1>`;
     
+    // Si la distancia del click es menor que 20px el juego finaliza y ganas.
     if(distance < 20){
-        $distance.innerHTML = `<h1>You has found the treasure in ${clicks} clicks!</h1>`;
+        $distance.innerHTML = `<h1>You has found the treasure in ${clicks} clicks. You Win!</h1>`;
+    } else if(clicks > 35){
+        return $distance.innerHTML = `<h1>You don't found the treasure in ${clicks} clicks. You Loss!</h1>`;
     }
 }); 
--- js/helpers.js
+++ js/helpers.js
@@ -1,13 +1,16 @@
+// Se genera un numero aleatorio entre 0 y el tamaño de WIDTH o HEIGHT que es lo que se pasara por parametro
 let getRandomNumber = (size) => {
     return Math.floor(Math.random() * size);
 }
 
+// Usando el teorema de pitagoras calculamos la distancia entre el click del usuario y el target(tesoro)
 let getDistance = (event, target) => {
     let diffX = event.offsetX - target.x;
     let diffY = event.offsetY - target.y;
     return Math.sqrt( (diffX * diffX) + (diffY *diffY) );
 }
 
+// Se generan pistas dependiendo de los pixeles a los que se encuentre el click
 let getDistanceHint = distance => {
     if (distance < 30) {
         return "Boiling Hot!";
